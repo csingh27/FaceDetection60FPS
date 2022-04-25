b5ec3ced85ffa40ae428094c1defb2e9add76e3d
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-04-25 23:03:36 +0200

    Read video

--- FaceDetection.py
+++ FaceDetection.py
@@ -1,4 +1,14 @@
 # A0. Import dependencies 
-import opencv
+import cv2
 import mediapipe as mp 
 import time 
+
+# A1. Face Detection
+##### A1.1 Read Video
+cap = cv2.VideoCapture("Videos/1.mp4")
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+    # cv2.waitKey(10) - Reduce Frame Rate
