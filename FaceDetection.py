9bb17c9f54d8aff8215c5304cffdffbdf512e3a3
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-04-30 22:08:46 +0200

    Display FPS on video

--- FaceDetection.py
+++ FaceDetection.py
@@ -5,10 +5,19 @@ import time
 
 # A1. Face Detection
 ##### A1.1 Read Video
-cap = cv2.VideoCapture("Videos/1.mp4")
+cap = cv2.VideoCapture("Videos/clip.wmv")
 
+pTime = 0
 while True:
     success, img = cap.read()
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
-    # cv2.waitKey(10) - Reduce Frame Rate
+    if(success):
+        # cv2.waitKey(1)
+        # A1.2 Display Frame Rate
+        cv2.waitKey(5) # - Reduce Frame Rate by increasing the wait value, More wait time implies lesser frame rate
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}',(20,70),cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 2)
+        cv2.imshow("Image",img)
+    else:
+        break;
--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 * Create a GitHub Account
 * Generate Personal Access Token from Github : Settings > Developer Settings > Personal Access Token > Generate new tokens  
 * Use your PAT as password when performing "git push" operation
+Crash Course on Git Fundamentals - https://www.youtube.com/watch?v=WB4M5-x33I0
+#### Vim
 
 ### A0. Install Project Dependencies  
 !pip install opencv-python
