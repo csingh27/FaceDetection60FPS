2b9427e529bfe2aa3fe9703703cab50a4255b087
Chandandeep Singh <singhchandandeep34@gmail.com>
2022-04-30 22:15:14 +0200

    Face Detection Process

--- FaceDetection.py
+++ FaceDetection.py
@@ -7,13 +7,25 @@ import time
 ##### A1.1 Read Video
 cap = cv2.VideoCapture("Videos/clip.wmv")
 
+
+# A2.1 
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection()
+
 pTime = 0
 while True:
     success, img = cap.read()
+
+    # A2.1
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+    print(results)
+
     if(success):
         # cv2.waitKey(1)
         # A1.2 Display Frame Rate
-        cv2.waitKey(5) # - Reduce Frame Rate by increasing the wait value, More wait time implies lesser frame rate
+        cv2.waitKey(1) # - Reduce Frame Rate by increasing the wait value, More wait time implies lesser frame rate
         cTime = time.time()
         fps = 1/(cTime-pTime)
         pTime = cTime
